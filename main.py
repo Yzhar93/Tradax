ba2b97134ece55064a16bf627811baefa34605e9
Yzhar <59482191+Yzhar@users.noreply.github.com>
2025-10-05 00:56:29 +0300

    add lambda function

---
+++ main.py
@@ -0,0 +1,11 @@
+from Tradax.bot.stock_fetcher import get_top_stocks
+from Tradax.bot.message_builder import build_message
+from Tradax.bot.telegram_client import send_telegram_message
+from Tradax.bot.llm_integration import enhance_message  # optional
+
+def stock_summary(event, context):
+    stocks = get_top_stocks()
+    msg = build_message(stocks)
+    msg = enhance_message(msg)  # optional Gemini integration
+    send_telegram_message(msg)
+    return {"status": "success"}
